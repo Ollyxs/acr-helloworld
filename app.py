8a8656490c85519476f308c62c96396b6e55130d
Lucas Ollarce <luk5ollarce@gmail.com>
2023-10-17 20:29:04 -0300

    fix log

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -22,26 +23,13 @@ load_dotenv()
 CONNECTION = "InstrumentationKey=0a00a0a0-00a0-0aaa-00aa-00aaa0aa00aa;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
 
 app = Flask(__name__)
-tracer_provider = TracerProvider(
-        resource=Resource.create({SERVICE_NAME: str(os.getenv('SERVICE', 'service'))})
-)
-trace.set_tracer_provider(
-    tracer_provider
-)
+app.config["OTEL_PYTHON_LOG_FORMAT"] = True
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 
 FlaskInstrumentor().instrument_app(app)
-
-RequestsInstrumentor().instrument()
-
-trace_exporter = AzureMonitorTraceExporter(
-    connection_string=os.getenv('APP_CONNECTION', CONNECTION)
-)
-
-tracer_provider.add_span_processor(
-    BatchSpanProcessor(trace_exporter)
-)
-
-app.logger.setLevel(logging.INFO)
+LoggingInstrumentor(set_logging_format=True).instrument()
 
 logger_provider = LoggerProvider()
 set_logger_provider(logger_provider)
@@ -53,9 +41,28 @@ log_exporter = AzureMonitorLogExporter(
 logger_provider.add_log_record_processor(
     BatchLogRecordProcessor(log_exporter)
 )
-handler = LoggingHandler()
+#handler = LoggingHandler()
 
-app.logger.addHandler(LoggingHandler())
+#app.logger.addHandler(LoggingHandler())
+
+tracer_provider = TracerProvider(
+        resource=Resource.create({SERVICE_NAME: str(os.getenv('SERVICE', 'service'))})
+)
+trace.set_tracer_provider(
+    tracer_provider
+)
+
+
+
+RequestsInstrumentor().instrument()
+
+trace_exporter = AzureMonitorTraceExporter(
+    connection_string=os.getenv('APP_CONNECTION', CONNECTION)
+)
+
+tracer_provider.add_span_processor(
+    BatchSpanProcessor(trace_exporter)
+)
 
 @app.route('/')
 def hello():
